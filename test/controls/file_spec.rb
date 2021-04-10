71731b3f2a2ec931e3e329f6cbcc9c47dc3dd8f2
wellyfox <freefox@gmail.com>
2021-04-10 22:12:12 +1200

    add impact for file_spec test

--- test/controls/file_spec.rb
+++ test/controls/file_spec.rb
@@ -1,4 +1,5 @@
 control 'welcome file is installed' do
+  impact 0.6
   describe bash("cat /home/ubuntu/welcome.txt") do
     its('exit_status') {should eq 0}
     its('stdout') {should match('WELCOME TO PACKER!')}
